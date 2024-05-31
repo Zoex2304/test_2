753e839674a71031efe97f1ffa3e01252954b729
Zoex2304 <134697481+Zoex2304@users.noreply.github.com>
2024-05-31 19:07:29 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>final project zikri</title>
+   <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../styling/style.css">
+  </head>
+  <body>
+  <?php 
+    include "../final project/navbar.html"
+  ?>
+    <script src="/bootstrap/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
